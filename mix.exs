30992629022c82dd133c729fdde4ab2876838716
Pablo Martínez <pablo.martinez.alvarez@gmail.com>
2017-08-18 18:30:01 +0200

    Mix updated for umbrella project

--- mix.exs
+++ mix.exs
@@ -2,24 +2,10 @@ defmodule Glayu.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :glayu,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     escript: escript_config(),
+    [apps_path: "apps",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
-  end
-
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [
-      mod: {GlayuApp, []},
-      extra_applications: [:yamerl, :logger, :eex]
-    ]
   end
 
   # Dependencies can be Hex packages:
@@ -30,25 +16,11 @@ defmodule Glayu.Mixfile do
   #
   #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
   #
-  # Type "mix help deps" for more examples and options
+  # Type "mix help deps" for more examples and options.
+  #
+  # Dependencies listed here are available only for this project
+  # and cannot be accessed from applications inside the apps folder
   defp deps do
-    [
-      {:yamerl, "~> 0.4.0"},
-      {:slugger, "~> 0.1.0"},
-      {:earmark, "~> 1.1.0"}, # 1.2.0 has concurrency problems (check https://github.com/pragdave/earmark/issues/147)
-      {:progress_bar, "> 0.0.0"},
-      {:poolboy, "~> 1.5.1"},
-      {:timex, "~> 3.0"},
-      {:saul, "~> 0.1"},
-      {:httpoison, "~> 0.11.1"},
-      {:tzdata, "== 0.1.8", override: true}, # forces an old tzdata version (check https://github.com/lau/tzdata/issues/24)
-      {:cowboy, "~> 1.0.0"},
-      {:plug, "~> 1.0"},
-      {:file_system, git: "https://github.com/pmartinezalvarez/file_system.git"} # :file_system escript support (https://github.com/falood/file_system/issues/34)
-    ]
-  end
-
-  defp escript_config do
-    [ main_module: Glayu.CLI ]
+    []
   end
 end
